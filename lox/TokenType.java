ab59231b9f0aaac5ba3108b63965aa4a27eb7d4f
DrPoppyseed <peaske16180@gmail.com>
2022-11-12 14:01:40 +0900

    initial commit

---
+++ lox/TokenType.java
@@ -0,0 +1,16 @@
+package lox;
+
+enum TokenType {
+  // Single-char token
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // one or two char token
+  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+  // literals
+  IDENTIFIER, STRING, NUMBER,
+
+  // keywords
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, SUPER, THIS, VAR, WHILE, EOF, RETURN, TRUE
+}
